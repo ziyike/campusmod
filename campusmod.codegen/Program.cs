3833ad3c08ca457ca898b35f8418df2f71e4be39
kzy <ziyike@outlook.com>
2024-10-26 17:54:29 +0800

    initial commit

---
+++ campusmod.codegen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace campusmod.codegen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ campusmod.resxgen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace campusmod.resxgen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
